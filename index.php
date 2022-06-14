98c31bc6801851fe793d5c3e5595902a7a8c5a84
RaiAn-CSE <68633815+RaiAn-CSE@users.noreply.github.com>
2022-06-14 22:28:09 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,271 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i&display=swap" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+		
+		<link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/4.5.6/css/ionicons.min.css">
+    <link rel="stylesheet" href="footer1/css/style-footer.css">
+		<link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="./Navbar/style.css">
+    <!-- raw css -->
+    <link rel="stylesheet" href="style.css">
+    <title>Paiso Ki</title>
+
+    <!-- fontawesome -->
+    <script src="https://kit.fontawesome.com/3b7a96cd6e.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+
+
+    <?php include("./Navbar/nav.html"); ?>
+
+    
+
+
+      <!-- main start -->
+    <main>
+      <section class="teacher">
+
+        <div class="custom-shape-divider-top-1636350828">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+              <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+          </svg>
+      </div>
+
+
+
+        <div class="custom-shape-divider-bottom-1636344393">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+              <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+          </svg>
+      </div>
+        <div class="inner-teacher">
+          <div class="left-teacher">
+            <div class="text mb-3">
+                <h3 class="teacher-text">Teacher information</h3>
+           </div>
+            <figure class="quote mt-4">
+              <blockquote class="blockquote">
+                <p>It is the supreme art of the teacher to awaken joy in creative expression and knowledge.</p>
+              </blockquote>
+              <figcaption class="blockquote-footer">
+                <cite title="Source Title">Albert Einstein</cite>
+              </figcaption>
+              
+            </figure>
+            <a role="button" href="teacher.html" class="btn btn-primary">Search <i class="fas fa-search"></i></a>
+          </div>
+          <div class="right-teacher">
+            <img src="./img/Teacher/teacher-removebg-preview-removebg-preview.png" alt="teacher">
+          </div>
+        </div>
+      </section>
+
+      <!-- teacher end -->
+        
+        
+         <!-- club start -->
+      <section class=" clubs pt-5 pb-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <h3 class="mb-3">Diu clubs </h3>
+                </div>
+                <div class="col-6 text-end">
+                    <a class="btn btn-primary mb-3 mr-1" href="#carouselExampleIndicators2" role="button" data-bs-slide="prev">
+                        <i class="fa fa-arrow-left"></i>
+                    </a>
+                    <a class="btn btn-primary mb-3 mr-1" href="#carouselExampleIndicators2" role="button" data-bs-slide="next">
+                        <i class="fa fa-arrow-right"></i>
+                    </a>
+                </div>
+                <div class="col-12">
+                    <div id="carouselExampleIndicators2" class="carousel slide" data-bs-ride="carousel">
+    
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <div class="row">
+    
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <img class="img-fluid" alt="100%x280" src="./img/club/1/1.jpg">
+                                            <div class="card-body">
+                                                <h4 class="card-title text-center">Computer & Programming Club</h4>
+                                                <p class="card-text">DIU CPC is the most primitive and extensive club as well as the biggest club in Daffodil International University. We work together to explore every field of Computer Science.</p>
+                                                <a href="http://cpc.daffodilvarsity.edu.bd/#" class="btn btn-primary mt-4">Explore more</a>
+    
+                                            </div>
+    
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <img class="img-fluid" alt="100%x280" src="./img/club/1/5.jpg">
+                                            <div class="card-body">
+                                                <h4 class="card-title text-center">GIRLS' COMPUTER PROGRAMMING CLUB</h4>
+                                                <p class="card-text">DIU GCPC is the first girls' computer programming club in Bangladesh. We started our journey from January 2018. </p>
+                                                <a href="http://cpc.daffodilvarsity.edu.bd/#" class="btn btn-primary mt-5">Explore more</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <img class="img-fluid" alt="100%x280" src="./img/club/1/8.jpg">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Diu Rotaract Club</h4>
+                                                <p class="card-text">This club one of the active and vibrant club in RID 3281 Bangladesh. Our focus area expanding professional networks, exchange ideas with leaders, and cultivate lifelong friendships around the world, develop skills to become community and global leaders.</p>
+                                                <a href="https://www.facebook.com/racdiu/" class="btn btn-primary mt-1">Explore more</a>
+                                            </div>
+                                        </div>
+                                    </div>
+    
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="row">
+    
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <img class="img-fluid" alt="100%x280" src="./img/club/1/3.jpg">
+                                            <div class="card-body">
+                                                <h4 class="card-title">DIU Debating club</h4>
+                                                <p class="card-text">Join to enhance your confidence to talk publicly.</p>
+                                                <a href="https://diudc.daffodilvarsity.edu.bd/" class="btn btn-primary">Explore more</a>
+                                            </div>
+    
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <img class="img-fluid" alt="100%x280" src="./img/club/1/2.jpg">
+                                            <div class="card-body">
+                                                <h4 class="card-title">DIU cyber security club</h4>
+                                                <p class="card-text">Prevent your system from unauthorized access.</p>
+                                                <a href="https://www.facebook.com/CSCDIU" class="btn btn-primary">Explore more</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <img class="img-fluid" alt="100%x280" src="./img/club/1/9.jpg">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Diu Textile club</h4>
+                                                <p class="card-text">Join  to develope practical skills.</p>
+                                                <a href="http://diutc.daffodilvarsity.edu.bd/" class="btn btn-primary mt-4">Explore more</a>
+                                            </div>
+                                        </div>
+                                    </div>
+    
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="row">
+    
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <img class="img-fluid" alt="100%x280" src="./img/club/1/6.jpg">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Diu NLP & ML Lab</h4>
+                                                <p class="card-text">Are you interested for research?</p>
+                                                <a href="https://www.facebook.com/diunlp" class="btn btn-primary">Explore more</a>
+                                            </div>
+    
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card h">
+                                            <img class="img-fluid" alt="100%x280" src="./img/club/1/4.jpg">
+                                            <div class="card-body">
+                                                <h4 class="card-title">English literary club</h4>
+                                                <p class="card-text">Are you develop your communication skill?</p>
+                                                <a href="https://www.facebook.com/diunlp" class="btn btn-primary">Explore more</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <img class="img-fluid" alt="100%x280" src="./img/club/1/8.jpg">
+                                            <div class="card-body">
+                                                <h4 class="card-title">DIU Robotics</h4>
+                                                <p class="card-text">Want to learn Robotics?</p>
+                                                <a href="https://www.facebook.com/drcdsc2018" class="btn btn-primary">Explore more</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- club end -->
+
+
+
+ <!-- question start -->
+ <section class="question">
+      <div class="container">
+        <div class="row mb-3">
+          <div class="col-md-6">
+            <div class="text">
+              <h3>DIU QUESTIONS</h3>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="qtn-btn text-end">
+              <a role="button" href="question.html" class="btn btn-primary">Find<i class="fas fa-search search"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="row second">
+          <div class="col-md-6">
+            <figure class="question-quote">
+              <blockquote class="blockquote">
+                <p>It is the supreme art of the teacher to awaken joy in creative expression and knowledge.</p>
+              </blockquote>
+              <figcaption class="blockquote-footer">
+                <cite title="Source Title">Albert Einstein</cite>
+              </figcaption>
+              
+            </figure>
+            
+          </div>
+          <div class="col-md-6">
+            <div class="right-question">
+              <img class="img-fluid" src="./img/Question/pressure.jpg" alt="teacher">
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </section>
+
+ <!-- question end -->
+
+<?php include("./footer1/index.html"); ?>
+
+        <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="./tilt.jquery.min.js"></script>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+
+  
+  </body>
+</html>
